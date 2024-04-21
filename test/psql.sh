8da72d2f2b1cbad9363c67da49eb8a2d52f973b8
HaoRan Ning <aichaosning@gmail.com>
2024-04-22 00:04:08 +1000

    merge

--- test/psql.sh
+++ test/psql.sh
@@ -1,13 +1,26 @@
-#以初始化用户进入psql
-sudo -u postgres psql
 
-# 通过 \l 查看所有数据库
-# \du 产看所有用户
+if [ "$1" == "start" ]; then
+    # 判断是否已经启动
+    sudo service postgresql status
+    # 如果没有启动，启动
+    sudo service postgresql start
+fi
+
+
+if [ "$1" == "new" ]; then
+    # 创建新表
+    CREATE DATABASE tpch
+    # 进入新表
+    \c tpch
+fi
+
+# 以初始化用户进入psql
+# sudo -u postgres psql 
 
 # 创建新表
 # CREATE DATABASE tpch
 
-# 进入新表
-\c tpch
 
-# 导入 表数据
+
+
+
